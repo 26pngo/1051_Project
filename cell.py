5f86acafbaa06356c933f1d12657a9f003b1ce23
26pngo <150211668+26pngo@users.noreply.github.com>
2023-12-11 23:53:28 -0500

    cell.py

--- cell.py
+++ cell.py
@@ -1,14 +1,19 @@
-from tkinter import Button
+from tkinter import Button, Label
 import random
-
 import settings
+import ctypes
+import sys
 
 
 class Cell:
     all = []
+    cell_count = settings.Cell_Count
+    cell_count_label_object = None
 
     def __init__(self, x, y, is_mine=False):
         self.is_mine = is_mine
+        self.is_opened = False
+        self.is_flag = False
         self.cell_btn_object = None
         self.x = x
         self.y = y
@@ -25,11 +30,29 @@ class Cell:
         btn.bind('<Button-3>', self.right_click_actions)
         self.cell_btn_object = btn
 
+    @staticmethod
+    def create_cell_count_label(location):
+        lbl = Label(
+            location,
+            bg='black',
+            fg='white',
+            text=f"Cells Left:{Cell.cell_count}",
+            font=("", 30)
+        )
+        Cell.cell_count_label_object = lbl
+
     def left_click_actions(self, event):
         if self.is_mine:
             self.show_mine()
         else:
+            if self.surrounded_cells_mines_length == 0:
+                for cell_obj in self.surrounded_cells:
+                    cell_obj.show_cell()
             self.show_cell()
+            if Cell.cell_count == settings.Mines_Count:
+                ctypes.windll.user32.MessageBoxW(0, 'You win', 'Game Over', 0)
+        self.cell_btn_object.unbind('<Button-1>')
+        self.cell_btn_object.unbind('<Button-3>')
 
     def get_cell_by_axis(self, x, y):
         for cell in Cell.all:
@@ -38,18 +61,12 @@ class Cell:
 
     @property
     def surrounded_cells(self):
-        cells = [
-            self.get_cell_by_axis(self.x - 1, self.y - 1),
-            self.get_cell_by_axis(self.x - 1, self.y),
-            self.get_cell_by_axis(self.x - 1, self.y + 1),
-            self.get_cell_by_axis(self.x, self.y - 1),
-            self.get_cell_by_axis(self.x + 1, self.y - 1),
-            self.get_cell_by_axis(self.x + 1, self.y),
-            self.get_cell_by_axis(self.x + 1, self.y + 1),
-            self.get_cell_by_axis(self.x, self.y + 1),
-        ]
+        cells = []
+        for i in range(-1, 2):
+            for j in range(-1, 2):
+                if self.get_cell_by_axis(self.x + i, self.y + j) is not None:
+                    cells.append(self.get_cell_by_axis(self.x + i, self.y + j))
 
-        cells = [cell for cell in cells if cell is not None]
         return cells
 
     @property
@@ -62,14 +79,35 @@ class Cell:
         return counter
 
     def show_cell(self):
-        self.cell_btn_object.configure(text = self.surrounded_cells_mines_length)
+        if not self.is_opened:
+            Cell.cell_count -= 1
+            self.cell_btn_object.configure(text=self.surrounded_cells_mines_length)
+            if Cell.cell_count_label_object:
+                Cell.cell_count_label_object.configure(
+                    text=f"Cells Left:{Cell.cell_count}"
+                )
+        self.cell_btn_object.configure(
+            bg='SystemButtonFace'
+        )
+
+        self.is_opened = True
 
     def show_mine(self):
         self.cell_btn_object.configure(bg='red')
+        ctypes.windll.user32.MessageBoxW(0, 'You clicked on a mine', 'You Lose', 0)
+        sys.exit()
 
     def right_click_actions(self, event):
-        print(event)
-        print("right")
+        if not self.is_flag:
+            self.cell_btn_object.configure(
+                bg='yellow'
+            )
+            self.is_flag = True
+        else:
+            self.cell_btn_object.configure(
+                bg='SystemButtonFace'
+            )
+            self.is_flag = False
 
     @staticmethod
     def randomize_mines():
